340e0cc27d3b726ea42e1b17cda539630b93219b
Doosies <song961003@gmail.com>
2021-08-07 14:34:38 +0900

    usestate 사용

--- src/StudyFolder/App.tsx
+++ src/StudyFolder/App.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
+import Counter from './Counter';
 import Hello from './Hello';
 
 const StyledApp = styled.div`
-    border: 2px solid black;
-    padding: 16px;
+
 `;
 
 const App = () => {
+
     return (
         <StyledApp>
-            <Hello name="react" color="red" isSpecial/>
-            <Hello color="pink"/>
+            <Counter/>
         </StyledApp>
     );
 };
